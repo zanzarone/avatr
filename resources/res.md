e06fb55aca60a69be577753fce30dced27647340
Samuele Scatena <samuele@MBP-di-Samuele.fritz.box>
2024-01-25 08:48:54 +0100

    ok

---
+++ resources/res.md
@@ -0,0 +1,10 @@
+https://decades404.github.io/react-pixelart-face-card-docs/
+
+icons
+https://www.streamlinehq.com/icons/pixel/users?search=&icon=ico_PrC6eI6szOq7BAbw
+
+bg palette
+https://kdesign.co/blog/pastel-color-palette-examples/
+
+box-shadow
+https://getcssscan.com/css-box-shadow-examples
--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import temp from "./style/icons/design-color-spray-2.png";
 
 function App() {
+  const [step, setStep] = useState(0);
+  const [bg, setBg] = useState(1);
+
+  const renderSwatches = () => {
+    const swatches = [
+      <div className={`swatch bg-1`} onClick={() => setBg(1)} />,
+      <div className={`swatch bg-2`} onClick={() => setBg(2)} />,
+      <div className={`swatch bg-3`} onClick={() => setBg(3)} />,
+      <div className={`swatch bg-4`} onClick={() => setBg(4)} />,
+      <div className={`swatch bg-5`} onClick={() => setBg(5)} />,
+      <div className={`swatch bg-6`} onClick={() => setBg(6)} />,
+    ];
+    return swatches;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="card">
+        <div className="header">
+          <h1 className="font-avatr">Avatar</h1>
+          <div>by</div>
+          <h3 className="">Samuele Scatena</h3>
+        </div>
+        <div className="container">
+          <div className={`pix bg-${bg}`}>
+            <div className="grid"></div>
+          </div>
+          <div className="controls">
+            <button>{"<"}</button>
+            <div className="selected-controls">
+              <img src={temp} width={24} height={24} />
+              <span>Background</span>
+            </div>
+            <button>{">"}</button>
+          </div>
+          <div className="controls-options">
+            {step === 0 && renderSwatches().map((item) => item)}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
