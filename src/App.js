2dbb231bf48805bdde7dcedd0ad0fcbe9e371767
samuele <samuele.scatena@gmail.com>
2024-01-30 17:35:59 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,106 +1,286 @@
 import React, { useState } from "react";
-import temp from "./style/icons/design-color-spray-2.png";
-import baseA from "./assets/base/baseA.png";
+
+const backgroundClasses = [
+  { style: "bg-1", value: 0 },
+  { style: "bg-2", value: 1 },
+  { style: "bg-3", value: 2 },
+  { style: "bg-4", value: 3 },
+  { style: "bg-5", value: 4 },
+  { style: "bg-6", value: 5 },
+];
+const skinClasses = [
+  { style: "skin-pale", value: 0, class: "pale" },
+  { style: "skin-white", value: 1, class: "white" },
+];
+
+const baseClasses = [
+  { value: 0, path: "/assets/base/type_A", text: "A-type" },
+  { value: 1, path: "/assets/base/type_B", text: "B-type" },
+  // { value: 1, path: "./assets/base/TypeA" },
+];
+
+const eyeClasses = [
+  { value: 0, path: "/assets/eyes/eyes1", text: "Type 1" },
+  // { value: 1, path: "./assets/base/TypeA" },
+];
+
+const earClasses = [
+  { value: 0, path: "/assets/ears/ears1", text: "Type 1" },
+  // { value: 1, path: "./assets/base/TypeA" },
+];
+
+const noseClasses = [
+  { value: 0, path: "/assets/nose/nose1", text: "Type 1" },
+  // { value: 0, path: "/assets/nose/nose-test", text: "Type 1" },
+  // { value: 1, path: "./assets/base/TypeA" },
+];
+
+const mouthClasses = [
+  { value: 0, path: "/assets/mouth/mouth1", text: "Type 1" },
+  // { value: 1, path: "./assets/base/TypeA" },
+];
+
+const hairClasses = [
+  { value: 0, path: "/assets/hair/hair1", text: "Type 1" },
+  // { value: 1, path: "./assets/base/TypeA" },
+];
+
+function generateRandomString(length) {
+  const charset =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    result += charset.charAt(randomIndex);
+  }
+
+  return result;
+}
+
+const Swatches = ({ classes, value, onColorChanged }) => {
+  return (
+    <div className="swatches">
+      {classes?.map((cl, index) => {
+        return (
+          <label key={generateRandomString(20)}>
+            <input
+              // value={cl.value}
+              checked={cl.value === value}
+              type="radio"
+              className="nes-radio"
+              // name="answer"
+              onChange={() => onColorChanged(index)}
+            />
+            <span
+              className={`nes-container is-rounded swatch ${cl.style}`}
+            ></span>
+          </label>
+        );
+      })}
+    </div>
+  );
+};
+
+const Select = ({ defaultOption, options, onChange }) => {
+  return (
+    <div className="nes-select">
+      <select
+        value={defaultOption}
+        required
+        key={generateRandomString(20)}
+        onChange={(e) => {
+          const found = options.find((o) => o.text === e.target.value);
+          console.log("pippo", e.target.value, found);
+          onChange(found?.value);
+          // onChange(e.target.value);
+        }}
+      >
+        {/* {!defaultOption && (
+          <option value="" disabled selected hidden>
+            Select...
+          </option>
+        )} */}
+        {options.map((option, index) => {
+          return (
+            <option
+              // key={`${generateRandomString(20)}_${index}`}
+              // selected={option.value === defaultOption}
+              value={option.text}
+            >
+              {option.text}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
 function App() {
-  const [step, setStep] = useState(0);
-  const [bg, setBg] = useState(1);
-
-  const renderSwatches = () => {
-    const swatches = [
-      <button
-        type="button"
-        className={`nes-btn btn-swtaches bg-1 nes-pointer`}
-        onClick={() => setBg(1)}
-      />,
-      <button
-        type="button"
-        className={`nes-btn btn-swtaches bg-2 nes-pointer`}
-        onClick={() => setBg(2)}
-      />,
-      <button
-        type="button"
-        className={`nes-btn btn-swtaches bg-3 nes-pointer`}
-        onClick={() => setBg(3)}
-      />,
-      <button
-        type="button"
-        className={`nes-btn btn-swtaches bg-4 nes-pointer`}
-        onClick={() => setBg(4)}
-      />,
-      <button
-        type="button"
-        className={`nes-btn btn-swtaches bg-5 nes-pointer`}
-        onClick={() => setBg(5)}
-      />,
-      <button
-        type="button"
-        className={`nes-btn btn-swtaches bg-6 nes-pointer`}
-        onClick={() => setBg(6)}
-      />,
-    ];
-    return swatches;
-  };
-
-  // return <div className="main">a</div>;
+  const [bg, setBg] = useState(0);
+  const [skinColor, setSkinColor] = useState(0);
+  const [faceType, setFaceType] = useState(0);
+  const [mouthType, setMouthType] = useState(0);
+  const [noseType, setNoseType] = useState(0);
+  const [eyesType, setEyesType] = useState(0);
+  const [earsType, setEarsType] = useState(0);
+  const [hairType, setHairType] = useState(0);
 
   return (
     <div className="main">
-      <div style={{ marginTop: 50 }} className="nes-container is-rounded card">
-        <div className="header">
-          <h1 className="font-joystix">Avatr</h1>
-          <a href="#" class="nes-badge is-splited">
-            <span class="is-dark">ver.</span>
-            <span class="is-primary">1.1.0</span>
-          </a>
-        </div>
-      </div>
-
-      <div className="nes-container is-rounded card">
-        <div className="container">
-          <div
-            style={{ display: "flex", flexDirection: "row", columnGap: 10 }}
-            className=""
-          >
-            <div style={{ display: "flex", flexDirection: "column", rowGap: 10 }}>
-              <div class="nes-container with-title">
-                <p class="title">Eyes</p>
-                <div class="nes-select">
-                  <select required id="default_select">
-                    <option value="" disabled selected hidden>
-                      Select...
-                    </option>
-                    <option value="0">To be</option>
-                    <option value="1">Not to be</option>
-                  </select>
+      <div className="container">
+        <div className="left">
+          <div className="nes-container is-rounded is-centered logo">
+            <h1 className="">
+              Avatr
+              <div className="app-info">
+                by Zanzarone
+                <div className="nes-badge is-splited">
+                  <span className="is-dark">ver</span>
+                  <span className="is-success">1.1.0</span>{" "}
                 </div>
               </div>
-              <div class="nes-container with-title">
-                <p class="title">Ears</p>
-              </div>
-            </div>
-            <div className={`nes-container is-rounded pix bg-${bg}`}>
-              <div className="base">
-                <img src={baseA} height={128} width={128} alt="" />
-              </div>
-            </div>
-            <div>
-              <div class="nes-container with-title">
-                <p class="title">Ears</p>
-              </div>
+            </h1>
+          </div>
+          <div className="nes-container with-title is-rounded">
+            <p className="title">Face</p>
+            <div className="full">
+              <Select
+                options={baseClasses}
+                defaultOption={baseClasses[faceType].text}
+                onChange={setFaceType}
+              />
             </div>
           </div>
-          <div style={{ width: "100%" }} class="nes-container with-title">
-            <p class="title">Background</p>
-            <div className="controls-options">
-              {step === 0 && renderSwatches().map((item) => item)}
+          <div className="nes-container with-title is-rounded">
+            <p className="title">Mouth</p>
+            <div className="full">
+              <Select
+                options={mouthClasses}
+                defaultOption={mouthType}
+                onChange={setMouthType}
+              />
+            </div>
+          </div>
+          <div className="nes-container with-title is-rounded">
+            <p className="title">Skin color</p>
+            <div className="full">
+              <Swatches
+                value={skinColor}
+                onColorChanged={(v) => setSkinColor(v)}
+                classes={skinClasses}
+              />
             </div>
           </div>
         </div>
-      </div>
-      <div className="nes-container is-rounded card">
-        <div className="header">
-          <div>by</div>
-          <div className="font-joystix">Samuele Scatena</div>
+        <div className="center">
+          <div
+            className={`nes-container with-title is-rounded avatar ${backgroundClasses[bg].style}`}
+          >
+            <p className="title">Preview</p>
+            <div className="preview">
+              <img
+                src={`${baseClasses[faceType].path}-${skinClasses[skinColor].class}.png`}
+                height={128}
+                width={128}
+                alt=""
+              />
+              <div className="eyes">
+                <img
+                  src={`${eyeClasses[eyesType].path}-${skinClasses[skinColor].class}.png`}
+                  height={128}
+                  width={128}
+                  alt=""
+                />
+              </div>
+              <div className="ears">
+                <img
+                  src={`${earClasses[earsType].path}-${skinClasses[skinColor].class}.png`}
+                  height={128}
+                  width={128}
+                  alt=""
+                />
+              </div>
+              <div className="mouth">
+                <img
+                  src={`${mouthClasses[mouthType].path}-${skinClasses[skinColor].class}.png`}
+                  height={128}
+                  width={128}
+                  alt=""
+                />
+              </div>
+              <div className="nose">
+                <img
+                  src={`${noseClasses[noseType].path}-${skinClasses[skinColor].class}.png`}
+                  height={128}
+                  width={128}
+                  alt=""
+                />
+              </div>
+              <div className="hair">
+                <img
+                  src={`${hairClasses[hairType].path}-${skinClasses[skinColor].class}.png`}
+                  height={128}
+                  width={128}
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div
+            className="nes-container with-title is-rounded"
+            style={{ width: 230 }}
+          >
+            <p className="title">Background</p>
+            <Swatches
+              value={bg}
+              classes={backgroundClasses}
+              onColorChanged={setBg}
+            />
+          </div>
+        </div>
+        <div className="right">
+          <div className="nes-container with-title is-rounded">
+            <p className="title">Eyes</p>
+            <div className="full">
+              <Select
+                options={eyeClasses}
+                defaultOption={eyesType}
+                onChange={setEyesType}
+              />
+            </div>
+          </div>
+          <div className="nes-container with-title is-rounded">
+            <p className="title">Ears</p>
+            <div className="full">
+              <Select
+                options={earClasses}
+                defaultOption={earsType}
+                onChange={setEarsType}
+              />
+            </div>
+          </div>
+          <div className="nes-container with-title is-rounded">
+            <p className="title">Nose</p>
+            <div className="full">
+              <Select
+                options={noseClasses}
+                defaultOption={noseType}
+                onChange={setNoseType}
+              />
+            </div>
+          </div>
+          <div className="nes-container with-title is-rounded">
+            <p className="title">Hair</p>
+            <div className="full">
+              <Select
+                options={hairClasses}
+                defaultOption={hairType}
+                onChange={setHairType}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
