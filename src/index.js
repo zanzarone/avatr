bf3a2c68dfa52e58c114bf76c050d3af7c255215
samuele <samuele.scatena@gmail.com>
2024-02-08 15:05:50 +0100

    Fixed production deployment, change avatar logo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style/general.scss";
 
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
